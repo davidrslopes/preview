079d5fc2f24c94011610788b74b40cd267d6dcd1
David Lopes <davidrslopes@gmail.com>
2018-09-12 17:19:12 +0100

    Folder Error Template

---
+++ templates/folder-error.php
@@ -0,0 +1,13 @@
+<!-- FOLDER ERROR ######################################## -->
+<section class="omnicp-error">
+  <div class="container">
+    <div class="row">
+      <div class="col">
+        <h1 class="display-1">404</h1>
+        <h3>A pasta que procura ainda não existe ou mudou de lugar.</h3>
+        <a href="/" class="btn btn-primary">VOLTAR AO INÍCIO</a>
+      </div>
+    </div>
+  </div>
+</section>
+<!-- //FOLDER ERROR ######################################## -->
